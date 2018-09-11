4b33c4bfd4e909133ee1a1cad33bb2d05fd9caa8
simra1311 <simraafreen.13@gmail.com>
2018-09-12 00:03:49 +0530

    icon added

--- app/src/main/java/com/example/android/mykaarma/activities/MapsActivity.java
+++ app/src/main/java/com/example/android/mykaarma/activities/MapsActivity.java
@@ -18,7 +18,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
